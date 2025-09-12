4ae4677001fe26f00c869c486f88ae4ab7744340
Çınar Atay <ataycinar06@gmail.com>
2025-09-12 11:37:40 +0300

    - Added courseRequest function which handles the API request for adding and removing courses with the provided token and CRNs.
    - Changed getToken function comments to a formal language

--- network_handler.py
+++ network_handler.py
@@ -6,16 +6,40 @@ import re
 ua = UserAgent()
 random_ua = ua.random
 
-def courseRequest(Ecrn,Scrn=[]):
-    # a function that handles the courseRequests through the API endpoint. #
-    return None
+# courseRequest function #
+# sends the provided CRNs both for adding and removing to the API endpoint without any delay. so if it gets called it will be sent to server immediately depending on your connection and server load #
+# parameters: 'Authorization' header as string, **CRNs you would like to add as a list of strings, **CRNs you would like to remove as a list of strings #
+# important !!! : added and removed CRN lists are both optional but one must be provided to function correctly #
+# output: response as string #
+# example output : {"ecrnResultList":[],"scrnResultList":[{"crn":"1111","operationFinished":true.... #
+
+def courseRequest(token,eCRN=[],sCRN=[]):
+
+    apiEndpoint = 'https://obs.itu.edu.tr/api/ders-kayit/v21'
+
+    # constructing the data for the api requests according to the provided eCRN and sCRN #
+    crnData = {'ECRN':eCRN , 'SCRN':sCRN} 
+
+    # defining the 'authorization' header as the provided bearer token and setting the user-agent #
+    authHeader = {'Authorization' : token, 'User-Agent': random_ua}  
+
+    # sending the request without any delay #
+    response = requests.post(apiEndpoint,json=crnData,headers=authHeader)
+
+    return response.text
+
+
+# getToken function #
+# parameters: ITUGiris username/email , ITUGiris password #
+# output: 'Authorization' header for provided user credentials #
+# example output -> 'Bearer QwNWFiYWIiLCJ...' 
 
 def getToken(username,password):
 
     # accessing OBS. #
     OBSfirstRequest = requests.get('https://obs.itu.edu.tr/login/auth/login?ReturnUrl=/ogrenci/') 
 
-    # redirects us to a ITU Giris url. #
+    # redirect to a ITU Giris url. #
     loginUrl = OBSfirstRequest.url 
 
     # parsing VIEWSTATE and EVENTVALIDATION from html since its a must. #
@@ -34,39 +58,39 @@ def getToken(username,password):
     'ctl00$ContentPlaceHolder1$btnLogin': 'Giriş / Login',
     }
 
-    # using the same random user-agent for request. #
+    # same random user-agent for request. #
     loginHeader= {'User-Agent' : random_ua} 
 
-    # sending user name and password for login. # # !!! 'cookieCheck' is mandatory and used for acknowledging with ITU Giris system that we will be using cookies from now on !!! #
+    # sending user name and password for login. # # !!! 'cookieCheck' is mandatory and used for acknowledging with ITU Giris system that cookies will be used from now on !!! #
     loginRequest = requests.post(loginUrl, data=loginData, headers=loginHeader, cookies={'cookieCheck' : 'true'}, allow_redirects=False) 
     
-    # we get a url to be redirected back to OBS again. #
+    # got a url to be redirected back to OBS again. #
     obsRedirectUrl=loginRequest.headers['Location']
 
     # accessing redirected url. #
     obsRedirectRequest = requests.get(obsRedirectUrl ,allow_redirects=False)
 
-    # we gathered a cookie called 'LoginCookie'. now we'll be parsing that from 'Set-Cookie' header. #
+    # gathered a cookie called 'LoginCookie'. parsing that from 'Set-Cookie' header. #
     obsRedirectCookies = obsRedirectRequest.headers['Set-Cookie']
 
-    match = re.search(r'LoginCookie=(.*?);', obsRedirectCookies) # with re library we are search a pattern where a substring starts with 'LoginCookie=' and ends with ';'. #
+    match = re.search(r'LoginCookie=(.*?);', obsRedirectCookies) # searching with re for a pattern where a substring starts with 'LoginCookie=' and ends with ';'. #
     if match: # if its found #
         LoginCookie = match.group(1) # assing that substring to variable. #
 
-    # now we should select our identity with a request using our 'LoginCookie'. #
+    # selecting the identity with a request using the 'LoginCookie'. #
     identityRequest = requests.get('https://obs.itu.edu.tr/login/SelectIdentity?returnURL=/ogrenci/', cookies={'LoginCookie':LoginCookie},allow_redirects=False)
 
-    # we got redirected to a identity url with a certain subSessionId other then the initial one. #
+    # got redirected to a identity url with a certain subSessionId other then the initial one. #
     identityRedirectUrl = 'https://obs.itu.edu.tr' + identityRequest.headers['Location']
 
     # accessing to that redirect url #
     setIdentityRequest = requests.get(identityRedirectUrl, cookies={'LoginCookie':LoginCookie}, allow_redirects=False)
 
-    # now we selected our identity and redirected back to OBS as a student. #
+    # selected the identity and redirected back to OBS as a student. #
     ogrenciRedirect = 'https://obs.itu.edu.tr' + setIdentityRequest.headers['Location']
     ogrenciLoginRequest = requests.get(ogrenciRedirect,cookies={'LoginCookie':LoginCookie},allow_redirects=False)
 
-    # we again gathered some cookies including 'OgrenciCookie' , which allows us to get our JWT by itself. #
+    # gathered some cookies including 'OgrenciCookie' , which allows to get our JWT by itself. #
     ogrenciAllCookies = ogrenciLoginRequest.headers['Set-Cookie']
 
     # doing the same search procedure but for 'OgrenciCookie'. #
@@ -75,8 +99,8 @@ def getToken(username,password):
     if match:
         OgrenciCookie = match.group(1)
     
-    # getting our JWT with 'OgrenciCookie'. we also put 'Bearer ' to the front because thats how we use it in headers. #
+    # getting users JWT with 'OgrenciCookie'. we also put 'Bearer ' to the front because thats how its used in headers. #
     jwt = 'Bearer ' + requests.get('https://obs.itu.edu.tr/ogrenci/auth/jwt',cookies={'OgrenciCookie':OgrenciCookie}).text
 
-    # ending our function #
+    # ending of function #
     return jwt
